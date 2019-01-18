a80c88e847c1f57628bfdc05ab5cb960c2d32eb7
silba4 <spezia2002@nate.com>
2019-01-18 12:43:45 +0900

    Updates

---
+++ precache-manifest.815bfe647a7e26462c6197564dfacdf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "273d4d6771fefbdbff53b470975ab9c6",
+    "url": "/norikizcafe/static/media/login1.273d4d67.jpg"
+  },
+  {
+    "revision": "f7d44be3f634df680c48",
+    "url": "/norikizcafe/static/js/runtime~main.f7d44be3.js"
+  },
+  {
+    "revision": "30a26140355dad6a642e",
+    "url": "/norikizcafe/static/js/main.30a26140.chunk.js"
+  },
+  {
+    "revision": "705489a8e9f5821a4d43",
+    "url": "/norikizcafe/static/js/1.705489a8.chunk.js"
+  },
+  {
+    "revision": "30a26140355dad6a642e",
+    "url": "/norikizcafe/static/css/main.ae75992c.chunk.css"
+  },
+  {
+    "revision": "b836e446b90d03f3cc810397e8f7f571",
+    "url": "/norikizcafe/index.html"
+  }
+];
